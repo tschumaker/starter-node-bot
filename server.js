0bd4b91b66477bc5e65902a3c4ea16c48c5f685c
tschumaker <troy.schumaker@gmail.com>
2016-09-19 15:17:41 -0600

    removed flows...and other event

--- server.js
+++ server.js
@@ -11,13 +11,41 @@ var slapp = Slapp({
   context: BeepBoopContext()
 })
 
-require('beepboop-slapp-presence-polyfill')(slapp, { debug: true })
-require('./flows')(slapp)
 var app = slapp.attachToExpress(express())
 
-app.get('/', function (req, res) {
-  res.send('Hello')
+slapp.message('^(hi|hello|hey)$', ['direct_mention', 'direct_message'], (msg, text) => {
+  msg
+    .say(text + ', how are you?')
+    .route(handleHowAreYou, {}, 60)
+})
+
+slapp.route(handleHowAreYou, (msg) => {
+  var resp = msg.body.event && msg.body.event.text
+
+  if (new RegExp('good', 'i').test(resp)) {
+    msg
+      .say(['Great! Ready?', ':smile: Are you sure?'])
+      .route(handleHowAreYou, 60)
+  } else {
+    msg.say('Me too')
+  }
+})
+
+slapp.message('^(thanks|thank you)', ['mention', 'direct_message'], (msg) => {
+  msg.say(['You are welcome', 'Of course'])
+})
+
+slapp.message('good night|bye', ['mention', 'direct_message'], (msg) => {
+  msg.say(['Cheers :beers:', 'Bye', 'Goodbye', 'Adios'])
+})
+
+slapp.message('.*', ['direct_mention', 'direct_message'], (msg) => {
+  // respond only 40% of the time
+  if (Math.random() < 0.4) {
+    msg.say([':wave:', ':pray:', ':raised_hands:'])
+  }
 })
 
 console.log('Listening on :' + process.env.PORT)
 app.listen(process.env.PORT)
+
